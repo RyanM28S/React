f6b2b0842ea1279e6aae5cf8489aa8a365e54b61
vinicius <vini976964150@gmail.com>
2026-04-27 11:26:09 -0300

    professores

--- meu-projeto-react/src/App.jsx
+++ meu-projeto-react/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './paginas/Home/Home.jsx'
 import CadastroP from './paginas/Formularios/CadastroP.jsx'
 import Footer from './componentes/footer/Footer.jsx'
 import Header from './componentes/Header/Header.jsx'
 import Inicial from './paginas/Inicial/Inicial.jsx'
+import Professores from './paginas/Professores/Professores.jsx'
 
 import Interface from './paginas/Interface/Interface.jsx'
 
@@ -22,9 +23,9 @@ const App = () => {
           <Route path="/" element={<Inicial />}/>
           <Route path="/cadastro" element={<CadastroP />}/>
           <Route path="/login" element={<Home />}/>
-          <Route path="/professores" element={<Home />}/>
+          <Route path="/professores" element={<Professores />}/>
           <Route path="/categorias" element={<Home />}/>
-          <Route path="/interface" element={<Home />}/>
+          <Route path="/interface" element={<Interface />}/>
         </Routes>
 
       </main>
