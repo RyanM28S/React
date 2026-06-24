79fae5c43c4de405be3c7127e25995018bcf1cb9
Lucas-Lopes-Alves <lucas.lopes7708@gmail.com>
2026-06-24 12:22:26 -0300

    a

--- BackEnd/rotas/registro.js
+++ BackEnd/rotas/registro.js
@@ -4,19 +4,30 @@ import db from "./db.js"
 const router = express.Router()
 
 async function registrarAlunos(req, res) {
-    const { nome, turma, data, descricao, notas } = req.body
-    if (!nome || !turma || !data || !descricao || !notas) {
+    const { nome, turma, ra, descricao, nota1, nota2, nota3, nota4 } = req.body
+    if (!nome || !turma || !ra || !descricao) {
         res.status(400).json({ message: "Falta informações" })
         return
     }
 
     try {
-        const registro = await db.query("INSERT INTO alunos(nome,turma,descricao,nota1,nota2,nota3,nota4,data) VALUES (?,?,?,?,?,?,?,?)",
-            [nome, turma, descricao, notas.nota1, notas.nota2, notas.nota3, notas.nota4, data])
+        const registro = await db.query("INSERT INTO alunos(nome,turma,descricao,ra) VALUES (?,?,?,?)",
+            [nome, turma, descricao, ra])
 
-        if (registro.affectedRows <= 0) {
+        if (registro.affectedRows < 1) {
             res.status(500).json({ message: "Não foi possivel registrar o aluno" })
             return
+        }
+        const pegar = await db.query("SELECT id FROM alunos WHERE ra = ?", [ra])
+        
+        if (pegar.length < 1) {
+            res.status(400).json({message: "Erro no banco de dados"})
+        }
+
+        const registroNotas = await db.query("INSERT INTO notasAlunos(id_aluno, nota_1,nota_2,nota_3,nota_4)",[pegar[0].id, nota1, nota2, nota3, nota4]) 
+
+        if (registroNotas.affectedRows < 1) {
+            res.status(500).json({message: "Não foi possivel regsitrar as notas"})
         }
         
         return res.status(201).json({message: "Aluno registrado com sucesso"})
