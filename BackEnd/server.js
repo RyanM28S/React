18f1c7d83cc2d15ee78398f23d74d776ba361727
Lucas-Lopes-Alves <lucas.lopes7708@gmail.com>
2026-07-01 12:16:29 -0300

    uns baguio

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import roteadorAcesso from './rotas/acesso.js'
 import roteadorRegistro from './rotas/registro.js'
 import roteadorAvaliacao from "./rotas/avaliacao.js"
+import rotaAvalicacao from "./rotas/avaliacao.js"
 
 const app = express()
 
@@ -11,6 +12,7 @@ app.use(cors())
 app.use(roteadorAcesso)
 app.use(roteadorRegistro)
 app.use(roteadorAvaliacao)
+app.use(rotaAvalicacao)
 
 app.listen(3001, ()=> {
     console.log("servidor rodando em http://localhost:3001");
--- DataBase/Opina_create.sql
+++ DataBase/Opina_create.sql
@@ -23,5 +23,22 @@ create table notasAlunos(
 	nota_3 int,
 	nota_4 int,
 	nota_final int generated always as ((nota_1+nota_2+nota_3+nota_4)/4) stored,
-	constraint fk_id_alunos foreign key(id_aluno) references alunos(id)n 
+	constraint fk_id_alunos foreign key(id_aluno) references alunos(id)
+);
+
+create table professores(
+	id int primary key auto_increment,
+	nome varchar(100),
+	area varchar(100),
+	email varchar(256) not null unique,
+	descricao longtext
+);
+
+create table avaliacoes(
+	id int primary key auto_increment,
+	id_professor int,
+	id_usuario int,
+	avaliacao longtext,
+	constraint fk_id_professor foreign key (id_professor) references professores(id),
+	constraint fk_id_usuario foreign key (id_usuario) references usuarios(id)
 );
